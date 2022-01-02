b2734435e2e107574d58e2338439ea36ddf2bc58
Web-Eye <Web-Eye@gmx.net>
2022-01-02 17:01:54 +0100

    bugfix, simplyfie

--- libs/emu/xbmcaddon.py
+++ libs/emu/xbmcaddon.py
@@ -21,21 +21,11 @@ class Addon:
         pass
 
     def getSetting(self, name):
-
         if name == 'quality':
             return 2
         elif name == 'start_page':
             return 1
 
     def getLocalizedString(self, id):
-        return {
-            30100: 'Home',
-            30101: 'Latest',
-            30102: 'Library',
-            30103: 'Most Watched',
-            30104: 'Top Movies',
-            30105: 'Opening This Week',
-            30106: 'Coming Soon',
-            30107: 'Navigations'
-        }[id]
+        return 'dummy'
 
